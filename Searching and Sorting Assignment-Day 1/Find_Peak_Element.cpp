f0701b7f8bdaeb2b76e5c787fd903eb26e0df377
Akhil Bachupally <63044342+Akhil66624@users.noreply.github.com>
2021-02-15 11:35:13 +0530

    updated
    updated

--- Searching and Sorting Assignment-Day 1/Find_Peak_Element.cpp
+++ Searching and Sorting Assignment-Day 1/Find_Peak_Element.cpp
@@ -1,5 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
+/*Naive Approach
+int findPeak(int arr[], int n)
+{
+    // first or last element is peak element
+    if (n == 1) 
+      return arr[0];
+    if (arr[0] >= arr[1])
+        return 0;
+    if (arr[n - 1] >= arr[n - 2])
+        return n - 1;
+ 
+    // check for every other element
+    for (int i = 1; i < n - 1; i++) {
+ 
+        // check if the neighbors are smaller
+        if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1])
+            return i;
+    }
+}
+ */
 findPeak(long long int arr[],long long int l,long long int h,long long int n)
 {
 	long long int mid=l+(h-l)/2;
